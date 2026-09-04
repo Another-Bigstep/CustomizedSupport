1dacfc7055581f3958d20bcd49e43ed523a79b19
Another-Bigstep <juneham421@gmail.com>
2026-09-04 14:26:31 +0900

    Update API URL and set demo mode to false

--- js/config.js
+++ js/config.js
@@ -3,7 +3,7 @@
  * apiUrl: Apps Script 웹앱 URL (…/exec). 비워 두면 데모 모드(브라우저 안의 가상 데이터)로 동작한다.
  */
 window.SOS_CONFIG = {
-  apiUrl: '',
+  apiUrl: 'https://script.google.com/macros/s/AKfycbywTA0CkxUOQCkkSXNFxeJkhvjkcJOF9WXjVGVXoQ_6oNt2e2ariV7AgaD2FaP2tjp7Pw/exec',
   appName: '학생맞춤통합지원 관찰관리',
-  demo: true
+  demo: false
 };
